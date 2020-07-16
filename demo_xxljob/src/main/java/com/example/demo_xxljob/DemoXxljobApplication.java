4999e5727c7a95e4c01d6b368e2f243c2ccaec00
wangyang2 <nbabob520@163.com>
2020-07-17 00:02:07 +0800

    没他妈成功，明天继续

---
+++ demo_xxljob/src/main/java/com/example/demo_xxljob/DemoXxljobApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_xxljob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoXxljobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoXxljobApplication.class, args);
+    }
+
+}
---
+++ demo_xxljob/src/test/java/com/example/demo_xxljob/DemoXxljobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_xxljob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoXxljobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
